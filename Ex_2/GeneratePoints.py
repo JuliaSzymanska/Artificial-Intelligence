5ea478ff8e8f4bd3b7f537e020ba106c2324e906
224441 Szymańska Julia <224441@edu.p.lodz.pl>
2020-05-04 12:54:39 +0200

    Generate points

---
+++ Ex_2/GeneratePoints.py
@@ -0,0 +1,29 @@
+from shapely.geometry import Polygon, Point
+import random
+
+def generator(poly, numberOfRandomPoints):
+    minx, miny, maxx, maxy = poly.bounds
+    listOfPoints = []
+    for i in range(numberOfRandomPoints):
+        while True:
+            p = Point(random.uniform(minx, maxx), random.uniform(miny, maxy))
+            if poly.contains(p):
+                # return p
+                listOfPoints.append(p)
+                break
+    return listOfPoints
+
+def findPoints():
+    numberOfPoints = int(input("Ile punktow bedzie posiadac figura? "))
+    points = []
+    for i in range(0, numberOfPoints):
+        x, y = input("Podaj x i y punktu: ").split()
+        points.append([])
+        points[i].append(float(x))
+        points[i].append(float(y))
+    p = Polygon(points)
+    numberOfRandomPoints = input("Ile losowych punktow wygenerowac?")
+    randomPoints = generator(p, int(numberOfRandomPoints))
+    individual_points = [(pt.x, pt.y) for pt in randomPoints]
+    with open("RandomPoints.txt", 'w') as f:
+        f.writelines(','.join(str(j) for j in i) + '\n' for i in individual_points)
---
+++ Ex_2/NeuralNetwork3.py
@@ -0,0 +1,11 @@
+import numpy as np
+import random
+import GeneratePoints
+
+
+class selfOrganizingMap(object):
+    def __init__(self):
+        self.pierwsz = 1
+
+
+GeneratePoints.findPoints()
