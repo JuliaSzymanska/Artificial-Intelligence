322d3e9916016c9e88c6fe2336b90e8ca68510a2
224441 Szymańska Julia <224441@edu.p.lodz.pl>
2020-05-21 20:14:53 +0200

    Added plot

--- Ex_3/NeuralNetwork3.py
+++ Ex_3/NeuralNetwork3.py
@@ -92,17 +92,34 @@ class NeuralNetwork(object):
         self.delta_weights_linear_layer = actual_output_adj
 
     def train(self, epoch_count):
+        error_test_data_plot = []
+        input_data_plot = []
+        output_data_plot = []
         combined_data = list(zip(self.input_data, self.expected_data))
         for epoch in range(epoch_count):
             self.epoch_error = 0.0
             np.random.shuffle(combined_data)
             for inp, outp in combined_data:
                 radial_layer_output, linear_layer_output = self.feed_forward(inp)
+                if epoch == epoch_count - 1:
+                    input_data_plot.append(inp[1])
+                    output_data_plot.append(*linear_layer_output)
                 self.backward_propagation(radial_layer_output, linear_layer_output, inp, outp)
                 # TODO: dzielic przez liczbe punktów
             self.epoch_error /= len(self.input_data)
             self.epoch_for_error.append(epoch)
             self.error_for_epoch.append(self.epoch_error)
+            # error_test_data_plot.append(self.test_network("approximation_test.txt"))
+            # self.plot_uni_graph("Błąd średniokwadratowy dla danych testowych", np.arange(0, epoch_count, 1),
+            #                     error_test_data_plot,
+            #                     "Epoki",
+            #                     "Wartość błędu")
+            # self.plot_uni_graph("Błąd średniokwadratowy", self.epoch_for_error, self.error_for_epoch, "Epoki",
+            #                     "Wartość błędu")
+            # self.plot_uni_graph_2_functions("Przebieg funkcji treningowej oraz jej aproksymacji", self.input_data[:, 1],
+            #                                 self.expected_data, "X", "Y", input_data_plot, output_data_plot,
+            #                                 "Funkcja treningowa")
+            # print("Blad dla treningowego: ", self.error_for_epoch[-1])
             print(epoch, "  ", self.epoch_error)
 
     def file_input(self, file_name):
@@ -124,11 +141,43 @@ class NeuralNetwork(object):
     #     plt.ylabel("Błąd średniokwadratowy")
     #     plt.xlabel("Epoka")
     #     plt.show()
+    #
+    # def plot_uni_graph(self, title, x_val, y_val, x_label, y_label):
+    #     plt.plot(x_val, y_val, 'ro', markersize=1)
+    #     plt.title(title)
+    #     plt.xlabel(x_label)
+    #     plt.ylabel(y_label)
+    #     plt.show()
+    #
+    # def plot_uni_graph_2_functions(self, title, x_val, y_val, x_label, y_label, x_val_1, y_val_1, function):
+    #     plt.plot(x_val, y_val, 'ro', markersize=1, label=function)
+    #     plt.plot(x_val_1, y_val_1, 'bo', markersize=1, label='Aproksymacja funkcji')
+    #     plt.title(title)
+    #     plt.legend()
+    #     plt.xlabel(x_label)
+    #     plt.ylabel(y_label)
+    #     plt.show()
+    #
+    # def test_network(self, test_file, is_graph=False):
+    #     test_data, expected_data = self.file_input(test_file)
+    #     test_data_bias = np.insert(test_data, 0, 1, axis=1)
+    #     input_data = np.stack((test_data_bias[:, 0], test_data_bias[:, 1]), axis=1)
+    #     # expected_data = test_data_bias[:, 2]
+    #     test_output = []
+    #     err = 0.0
+    #     for test_pair in input_data:
+    #         hidden_layer_output_test, output_layer_output_test = self.feed_forward(test_pair)
+    #         test_output.append(output_layer_output_test)
+    #     for i in range(len(test_output)):
+    #         err += (test_output[i] - expected_data[i]) ** 2
+    #     err /= 2
+    #     if is_graph:
+    #         self.plot_uni_graph_2_functions("Przebieg funkcji testowej oraz jej aproksymacji", test_data[:, 0],
+    #                                         test_data[:, 1], "X",
+    #                                         "Y", test_data[:, 0], test_output, "Funkcja testowa")
+    #     return (err / len(test_output))
 
 
-NeuNet = NeuralNetwork(4, 1, "approximation_2.txt", 1)
+NeuNet = NeuralNetwork(4, 1, "approximation_1.txt", 1)
 NeuNet.train(2000)
-# print("Wynik:")
-# for i in NeuNet.input_data:
-#     print(NeuNet.feed_forward(i, True)[1])
-# NeuNet.graph()
+# print("Blad dla testowego: ", NeuNet.test_network("approximation_test.txt", True))
