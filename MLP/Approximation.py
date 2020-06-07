dbed81c032b9718ac25c20739de5d17f6cde739f
224441 Szymańska Julia <224441@edu.p.lodz.pl>
2020-06-08 00:10:50 +0200

    Added plots for approximation

--- MLP/Approximation.py
+++ MLP/Approximation.py
@@ -2,56 +2,60 @@ import numpy as np
 import matplotlib.pyplot as plt
 import csv
 
-learning_coeff = 0.45
-
+learning_coeff = 0.1
 epoch_error = 0.0
 
-momentum_coeff = 0.9
+momentum_coeff = 0.1
 
 
-class NeuralNetwork(object):
-    def __init__(self, number_of_input, number_of_hidden, number_of_output, input_data_file, expected_data_file,
-                 is_bias=0):
-        np.random.seed(30)
+class NeuralNetworkApproximation(object):
+    def __init__(self, number_of_input, number_of_hidden, number_of_output, input_data_file, is_bias=0):
+        np.random.seed(2)
         self.hidden_layer_weights = []
         self.output_layer_weights = []
         self.delta_weights_output_layer = []
         self.delta_weights_hidden_layer = []
-        self.initialze_weights(is_bias, number_of_hidden, number_of_input, number_of_output)
+        self.input_data = []
+        self.expected_data = []
+        self.initialze_weighs(is_bias, number_of_hidden, number_of_input, number_of_output)
         self.is_bias = is_bias
         self.epoch_error = 0.0
         self.error_for_epoch = []
         self.epoch_for_error = []
-        self.input_data = self.file_input(input_data_file)
-        self.expected_data = self.file_input(expected_data_file)
+        self.data = self.file_input(input_data_file)
         self.resolve_bias()
 
-    def sigmoid_function(self, x):
-        result = 1 / (1 + np.exp(-x))
-        return result
 
-    def sigmoid_derivative(self, x):
-        return x * (1 - x)
 
-    def feed_forward(self, input_data, is_training=True):
-        if not is_training and self.is_bias:
-            input_data = np.insert(input_data, 0, 1)
-        hidden_layer_output = self.sigmoid_function(np.dot(input_data, self.hidden_layer_weights))
+    def linear_func(self, x):
+        return x
+
+    def linear_derivative(self, x):
+        return 1
+
+    def sigmoid_func(self, x):
+        output = 1 / (1 + np.exp(-x))
+        return output
+
+    def sigmoid_derivative(self, z):
+        return z * (1 - z)
+
+    def feed_forward(self, input_data):
+        hidden_layer_output = self.sigmoid_func(np.dot(input_data, self.hidden_layer_weights))
         if self.is_bias == 1:
             hidden_layer_output = np.insert(hidden_layer_output, 0, 1)
-        output_layer_output = self.sigmoid_function(np.dot(hidden_layer_output, self.output_layer_weights))
+        output_layer_output = self.linear_func(np.dot(hidden_layer_output, self.output_layer_weights))
         return hidden_layer_output, output_layer_output
 
     def backward_propagation(self, hidden_layer_result, output_layer_result, input_data, output_data):
         avr_err = 0.0
         output_difference = output_layer_result - output_data
-
         for i in output_difference:
             avr_err += i ** 2
         avr_err /= 2
         self.epoch_error += avr_err
 
-        delta_coefficient_outp = output_difference * self.sigmoid_derivative(output_layer_result)
+        delta_coefficient_outp = output_difference * self.linear_derivative(output_layer_result)
         hidden_layer_error = delta_coefficient_outp.dot(self.output_layer_weights.T)
         if self.is_bias == 1:
             hidden_layer_error = hidden_layer_error[1:]
@@ -65,7 +69,6 @@ class NeuralNetwork(object):
             output_adj.append(hidden_layer_result * i)
         for i in delta_coefficient_hidden:
             hidden_adj.append(input_data * i)
-
         hidden_adj = np.asarray(hidden_adj)
         output_adj = np.asarray(output_adj)
 
@@ -74,26 +77,69 @@ class NeuralNetwork(object):
         self.hidden_layer_weights -= actual_hidden_adj
         self.output_layer_weights -= actual_output_adj
 
-
         self.delta_weights_hidden_layer = actual_hidden_adj
         self.delta_weights_output_layer = actual_output_adj
 
     def train(self, epoch_number):
+        error_test_data_plot = []
+        input_data_plot = []
+        output_data_plot = []
         combined_data = list(zip(self.input_data, self.expected_data))
         for epoch in range(epoch_number):
             self.epoch_error = 0.0
             np.random.shuffle(combined_data)
             for inp, outp in combined_data:
+                if self.is_bias == 0:
+                    inp = np.asarray([inp])
+                outp = np.asarray([outp])
                 hidden_layer_output, output_layer_output = self.feed_forward(inp)
+                if epoch == epoch_number - 1:
+                    input_data_plot.append(inp[1])
+                    output_data_plot.append(*output_layer_output)
                 self.backward_propagation(hidden_layer_output, output_layer_output, inp, outp)
-            self.epoch_error /= 4
+            self.epoch_error /= len(self.input_data)
             self.epoch_for_error.append(epoch)
             self.error_for_epoch.append(self.epoch_error)
-        for i in self.input_data:
-            print(self.feed_forward(i, True)[1])
-        self.graph()
+            error_test_data_plot.append(self.test_network("Approximation_data_test.txt"))
+        self.plot_uni_graph("Mean square error for test data", np.arange(0, epoch_number, 1),
+                            error_test_data_plot,
+                            "Epoch",
+                            "Error value")
+        self.plot_uni_graph("Mean square error", self.epoch_for_error, self.error_for_epoch, "Epoch",
+                            "Error value")
+        self.plot_uni_graph_2_functions("Training function and its approximation", self.input_data[:, 1],
+                                        self.expected_data, "X", "Y", input_data_plot, output_data_plot,
+                                        "Training function")
+        print("Error for training data: ", self.error_for_epoch[-1])
+        print("Error for testing data: ", network.test_network("Approximation_data_test.txt", True))
 
-    def initialze_weights(self, is_bias, number_of_hidden, number_of_input, number_of_output):
+    def file_input(self, file_name):
+        with open(file_name) as f:
+            input_val = []
+            expected_val = []
+            data = csv.reader(f, delimiter=' ')
+            for row in data:
+                input_val.append(float(row[0]))
+                expected_val.append(float(row[1]))
+        return np.stack((input_val, expected_val), axis=1)
+
+    def plot_uni_graph(self, title, x_val, y_val, x_label, y_label):
+        plt.plot(x_val, y_val, 'ro', markersize=1)
+        plt.title(title)
+        plt.xlabel(x_label)
+        plt.ylabel(y_label)
+        plt.show()
+
+    def plot_uni_graph_2_functions(self, title, x_val, y_val, x_label, y_label, x_val_1, y_val_1, function):
+        plt.plot(x_val, y_val, 'ro', markersize=1, label=function)
+        plt.plot(x_val_1, y_val_1, 'bo', markersize=1, label='Approximation of functions')
+        plt.title(title)
+        plt.legend()
+        plt.xlabel(x_label)
+        plt.ylabel(y_label)
+        plt.show()
+
+    def initialze_weighs(self, is_bias, number_of_hidden, number_of_input, number_of_output):
         self.hidden_layer_weights = 2 * np.random.random((number_of_input + is_bias, number_of_hidden)) - 1
         self.delta_weights_hidden_layer = np.zeros((number_of_input, number_of_hidden))
         self.output_layer_weights = 2 * np.random.random((number_of_hidden + is_bias, number_of_output)) - 1
@@ -101,29 +147,36 @@ class NeuralNetwork(object):
 
     def resolve_bias(self):
         if self.is_bias == 1:
-            self.input_data = np.insert(self.input_data, 0, 1, axis=1)
+            self.data = np.insert(self.data, 0, 1, axis=1)
             self.delta_weights_hidden_layer = np.insert(self.delta_weights_hidden_layer, 0, 0, axis=0)
             self.delta_weights_output_layer = np.insert(self.delta_weights_output_layer, 0, 0, axis=0)
+            self.input_data = np.stack((self.data[:, 0], self.data[:, 1]), axis=1)
+            self.expected_data = self.data[:, 2]
+        else:
+            self.input_data = self.data[:, 0]
+            self.expected_data = self.data[:, 1]
 
-    def file_input(self, file_name):
-        input_arr = []
-        with open(file_name, "r") as f:
-            data = csv.reader(f, delimiter=' ')
-            for row in data:
-                tmp_arr = []
-                for i in row:
-                    tmp_arr.append(float(i))
-                input_arr.append(tmp_arr)
-        return np.asarray(input_arr)
+    def test_network(self, test_file, is_graph=False):
+        test_data = self.file_input(test_file)
+        test_data_bias = np.insert(test_data, 0, 1, axis=1)
+        input_data = np.stack((test_data_bias[:, 0], test_data_bias[:, 1]), axis=1)
+        expected_data = test_data_bias[:, 2]
+        test_output = []
+        err = 0.0
+        for test_pair in input_data:
+            hidden_layer_output_test, output_layer_output_test = self.feed_forward(test_pair)
+            test_output.append(output_layer_output_test)
+        for i in range(len(test_output)):
+            err += (test_output[i] - expected_data[i]) ** 2
+        err /= 2
+        if is_graph:
+            self.plot_uni_graph_2_functions("Testing function and its approximation", test_data[:, 0],
+                                            test_data[:, 1], "X",
+                                            "Y", test_data[:, 0], test_output, "Testing function")
+        return (err / len(test_output))
 
-    def graph(self):
-        plt.plot(self.epoch_for_error, self.error_for_epoch, 'ro', markersize=1)
-        plt.title("Mean square error for the epoch")
-        plt.ylabel("Square error")
-        plt.xlabel("Epoch")
-        plt.show()
 
-
-network = NeuralNetwork(number_of_input=4, number_of_hidden=3, number_of_output=4, input_data_file="transformation_data.txt",
-                        expected_data_file="transformation_data.txt", is_bias=1)
+network = NeuralNetworkApproximation(number_of_input=1, number_of_hidden=10, number_of_output=1,
+                                     input_data_file="Approximation_data_1.txt", is_bias=1)
 network.train(epoch_number=2000)
+
--- MLP/README.md
+++ MLP/README.md
@@ -1,12 +1,41 @@
 ## [MLP](https://github.com/JuliaSzymanska/Artificial-Intelligence/tree/master/MLP)
 This task consists of two parts, each of which presents one example of how to use the multilayer perceptron (MLP).
+
 ### [Approximation](https://github.com/JuliaSzymanska/Artificial-Intelligence/blob/master/MLP/Approximation.py)
 Implementation of a neural network with one input layer, one linear hidden layer, and one output layer with a sigmoidal activation function.
 
 #### USAGE
 * Create a network instance
 ```python
-network = NeuralNetwork(number_of_input=4, number_of_hidden=3, number_of_output=4, input_data_file="transformation.txt",
+network = NeuralNetwork(number_of_input=1, number_of_hidden=10, number_of_output=1, 
+                        input_data_file="Approximation_data_1.txt", is_bias=1)
+```
+* Train network
+```python
+network.train(epoch_number=2000)
+```
+
+#### Output
+* Mean square error plot for training data
+Example plot:
+![ApproximationPlot.png](https://github.com/JuliaSzymanska/Artificial-Intelligence/blob/master/MLP/ApproximationPlot.png)
+
+* Output data on over-trained network
+For input:
+```text
+1 0 0 0
+0 1 0 0
+0 0 1 0
+0 0 0 1
+```
+
+### [Classification](https://github.com/JuliaSzymanska/Artificial-Intelligence/blob/master/MLP/Classification.py)
+Implementation of a neural network with input layer with 1 to 4 neurons, one sigmoidal hidden layer, and one output layer with 3 neurons with a sigmoidal activation function.
+
+#### USAGE
+* Create a network instance
+```python
+network = NeuralNetworkApproximation(number_of_input=4, number_of_hidden=3, number_of_output=4, input_data_file="transformation.txt",
                         expected_data_file="transformation.txt", is_bias=1)
 ```
 * Train network
@@ -17,7 +46,7 @@ network.train(epoch_number=2000)
 #### Output
 * Mean square error plot
 Example plot:
-![MeanSquareError.png](https://github.com/JuliaSzymanska/Artificial-Intelligence/blob/master/MLP/MeanSquareError.png)
+![ApproximationPlot.png](https://github.com/JuliaSzymanska/Artificial-Intelligence/blob/master/MLP/ApproximationPlot.png)
 
 * Output data on over-trained network
 For input:
@@ -34,7 +63,3 @@ Example results:
 [0.01063865 0.00948483 0.98665415 0.00992539]
 [0.00608939 0.0063894  0.00533386 0.98834747]
 ```
-
-
-### [Classification](https://github.com/JuliaSzymanska/Artificial-Intelligence/blob/master/MLP/Classification.py)
-
